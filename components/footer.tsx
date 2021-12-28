9c399263b4c573735a4b4bf380f4af9a6ba30c8b
Ian Culp <icculp@gmail.com>
2021-12-27 20:58:02 -0600

    leaving object

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,31 +19,22 @@ import VercelLogo from '@components/icons/icon-platform';
 import styles from './footer.module.css';
 import { COPYRIGHT_HOLDER, SITE_NAME, CODE_OF_CONDUCT, LEGAL_URL, REPO } from '@lib/constants';
 
-/**
+
 export function HostedByVercel() {
   return (
-    <a
-      href="https://vercel.com"
-      className={cn(styles['footer-link'], styles['footer-logo'])}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <div className={styles['secondary-text']}>Created by </div>
-      <VercelLogo color="white" />
-    </a>
+      <div className={styles['secondary-text']}></div>
   );
 }
-42-45
-        <div className={styles['footer-hostedby']}>
-          <HostedByVercel />
-          <div className={styles['footer-separator']} />
-        </div>
-*/
+
 
 export default function Footer() {
   return (
     <footer className={cn(styles.footer)}>
       <div className={styles['footer-legal']}>
+        <div className={styles['footer-hostedby']}>
+          <HostedByVercel />
+          <div className={styles['footer-separator']} />
+        </div>
         <div className={styles['footer-copyright']}>
           Copyright © {`${new Date().getFullYear()} `} {COPYRIGHT_HOLDER || `${SITE_NAME}.`} All
           rights reserved.
