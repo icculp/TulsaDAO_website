fee4c15a777baf86f2e5b4bedef5d4ce2cfd249e
Ian Culp <icculp@gmail.com>
2021-12-27 21:13:39 -0600

    remove div from layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -39,10 +39,7 @@ export default function Footer() {
   return (
     <footer className={cn(styles.footer)}>
       <div className={styles['footer-legal']}>
-        <div className={styles['footer-hostedby']}>
-          <HostedByVercel />
           <div className={styles['footer-separator']} />
-        </div>
         <div className={styles['footer-copyright']}>
           Copyright © {`${new Date().getFullYear()} `} {COPYRIGHT_HOLDER || `${SITE_NAME}.`} All
           rights reserved.
